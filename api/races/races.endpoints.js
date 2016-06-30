16a977bc22c313a986d5c88c7fb0971e7c39cbdc
DaveOrDead <davidajberner@gmail.com>
2016-06-30 12:20:40 +0100

    add get race

--- api/races/races.endpoints.js
+++ api/races/races.endpoints.js
@@ -22,3 +22,30 @@ exports.index = function(request, response) {
   });
 };
 
+exports.read = function(request, response) {
+
+    var race_id = request.params.id;
+
+    // Get all people for race
+    var race_query =
+    "select r.id, r.name, s.name as size, \
+        ( \
+        select s.name as size \
+        from LU_sizes s \
+        where r.size_id = s.id \
+        ) \
+    from LU_races r \
+    where r.id = " + race_id;
+
+
+  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+    client.query( race_query, function(err, result) {
+      done();
+      if (err)
+       { console.error(err); response.send("Error " + err); }
+      else
+       { response.send(result.rows); }
+    });
+  });
+};
+
