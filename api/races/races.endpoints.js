1f633b6be2b80dd5ce862fcd6d0b9b98c7ca89a3
DaveOrDead <davidajberner@gmail.com>
2016-06-30 15:44:11 +0100

    endpoints test

--- api/races/races.endpoints.js
+++ api/races/races.endpoints.js
@@ -28,15 +28,15 @@ exports.read = function(request, response) {
 
     // Get all people for race
     var race_query =
-    "select r.id, r.name, \
-        ( \
-        select s.name as size \
-        from LU_sizes s \
-        where r.size_id = s.id \
-        ) \
-    from LU_races r \
-    where r.id = " + race_id;
-
+    `select r.id, r.name, ram.ability_id, ram.modifier,
+        (
+        select s.name as size
+        from LU_sizes s
+        where r.size = s.id
+        )
+    from LU_races r, race_ability_modifiers ram
+    where r.id = ram.race_id
+    and r.id = ${race_id}`;
 
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
     client.query( race_query, function(err, result) {
@@ -44,7 +44,12 @@ exports.read = function(request, response) {
       if (err)
        { console.error(err); response.send("Error " + err); }
       else
-       { response.send(result.rows); }
+       // {
+       //     for (var i = 0, max = result.rows.length ; i < max; i++) {
+       //     	result[i]
+       //     }
+
+       	response.send(result.rows); }
     });
   });
 };
