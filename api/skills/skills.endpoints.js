ca5f26252d28ecad47bc69176297f5bb085d11eb
DaveOrDead <davidajberner@gmail.com>
2016-07-02 10:48:42 +0100

    add ability_id to skill results

--- api/skills/skills.endpoints.js
+++ api/skills/skills.endpoints.js
@@ -12,7 +12,7 @@ var pg = require('pg');
 exports.index = function(request, response) {
 
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query("select id, name from lu_skills", function(err, result) {
+    client.query("select id, name, ability_id from lu_skills", function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
