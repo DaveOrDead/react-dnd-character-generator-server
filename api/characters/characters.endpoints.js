8c9fc496b809475e5f6328209b004bd121e5b6c5
DaveOrDead <davidajberner@gmail.com>
2016-07-09 00:17:50 +0100

    character details

--- api/characters/characters.endpoints.js
+++ api/characters/characters.endpoints.js
@@ -11,8 +11,21 @@ var pg = require('pg');
 // Get list of characters
 exports.index = function(request, response) {
 
+    var charactersQuery = "select c.id, c.name, c.avatar, \
+    ( \
+        select r.name as race \
+        from LU_races r \
+        where r.id = c.race_id \
+    ), \
+    ( \
+        select cl.name as class \
+        from LU_classes cl \
+        where r.id = c.class_id \
+    ) \
+    from characters c";
+
   pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query("select * from characters", function(err, result) {
+    client.query(charactersQuery, function(err, result) {
       done();
       if (err)
        { console.error(err); response.send("Error " + err); }
