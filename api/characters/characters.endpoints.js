5cf482ecf4e1ff475a400693da2274947de15bb6
DaveOrDead <davidajberner@gmail.com>
2016-07-28 13:42:19 +0100

    add schema and character files

--- api/characters/characters.endpoints.js
+++ api/characters/characters.endpoints.js
@@ -69,13 +69,32 @@ exports.read = function(request, response) {
     from characters c \
     where c.id = '" + character_id + "'";
 
-  pg.connect(process.env.DATABASE_URL, function(err, client, done) {
-    client.query(getCharacterQuery, function(err, result) {
-      done();
-      if (err)
-       { console.error(err); response.send("Error " + err); }
-      else
-       { response.send(result.rows[0]); }
-    });
+    pg.connect(process.env.DATABASE_URL, function(err, client, done) {
+        client.query(getCharacterQuery, function(err, result1) {
+        done();
+        if (err) {
+            console.error(err);
+            response.send("Error " + err);
+        } else {
+
+            // Get all details for character
+            var abilitiesQuery = "select ability_id, value \
+            from character_abilities \
+            where character_id = '" + character_id + "'";
+
+            client.query(abilitiesQuery, function(err, result2) {
+                done();
+                if (err) {
+                    console.error(err); response.send("Error " + err);
+                } else {
+                    result1.rows[0].abilities = {};
+                    for (var i = 0, max = result2.rows.length; i < max; i++) {
+                        result1.rows[0].modifiers[result2.rows[i].ability_id] = result2.rows[i].modifier;
+                    }
+
+                    response.send(result1.rows);
+                }
+            });
+        }
   });
 };
