75e7b2750505efa961c67de3574ad48413dcc164
DaveOrDead <davidajberner@gmail.com>
2016-07-28 19:24:46 +0100

    use new classes api

--- api/classes/classes.endpoints.js
+++ api/classes/classes.endpoints.js
@@ -6,21 +6,30 @@
 const req = require('../../requests');
 
 const index = (request, response, next) => {
-    const query = 'select id, name from lu_classes';
+    const query = 'select id, name from character_classes limit 11';
+    // TODO: clean this up and do by rule set rather than 11.
     req.getAll(request, response, next, query);
 };
 
 const read = (request, response, next) => {
     const params = [request.params.id, request.params.level || 'lvl1'];
+    // const query = `
+    //     select c.id, c.name, c.hit_die,
+    //     c.initial_skill_modifier, r.level_skill_modifier,
+    //     cl.base_attack_bonus, cl.fortitude_save,
+    //     cl.reflex_save, cl.will_save
+    //     from LU_classes r, LU_class_levels cl
+    //     where r.id = $1
+    //     and cl.class_id = $1
+    //     and cl.level_id = $2`;
+    //
     const query = `
-        select r.id, r.name, r.hit_die,
-        r.initial_skill_modifier, r.level_skill_modifier,
-        cl.base_attack_bonus, cl.fortitude_save,
-        cl.reflex_save, cl.will_save
-        from LU_classes r, LU_class_levels cl
-        where r.id = $1
-        and cl.class_id = $1
-        and cl.level_id = $2`;
+        select cc.id, cc.name, ccv.hit_die,
+        ccv.skill_points, ccv.starting_gold
+        from character_class_variant ccv, character_class cc
+        where ccv.rulebook_id = 6
+        where ccv.character_class_id = $1
+        and ccv.character_class_id = $1`;
 
     req.getOne(request, response, next, query, params);
 };
