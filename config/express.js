9def225584e6e81d93e3ca9c9ad8835343a1d55a
DaveOrDead <davidajberner@gmail.com>
2016-07-28 17:31:30 +0100

    es6 cleanup

--- config/express.js
+++ config/express.js
@@ -7,7 +7,7 @@ const express = require('express');
 // needed to extract data from request.body in the endpoints
 const bodyParser = require('body-parser');
 
-module.exports = function(app) {
+module.exports = app => {
     app.use(bodyParser.urlencoded({ extended: false }));
     app.use(bodyParser.json());
 };
